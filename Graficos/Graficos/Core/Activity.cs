1091268d961b931ef0fc2453e7649f194c673c4c
elsanchez8@gmail.com <elsanchez8@gmail.com>
2016-11-08 18:54:57 +0100

    Creada Parte 1 Gráficos , sin terminar de pulir , trataré de terminarla hoy noche o mañana.  Faltan comentarios , cambiar tamaño y solucionar pequeño fallo en tratamiento de actividades

---
+++ Graficos/Graficos/Core/Activity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Graficos
+{
+	public class Activity
+	{
+
+		private DateTime date;
+		private int duration;
+		private int distance;
+
+		public int GetDuration()
+		{
+			return duration;
+		}
+		public int GetDistance()
+		{
+			return distance;
+		}
+		public DateTime GetDate()
+		{
+			return date;
+		}
+
+		public Activity(DateTime day, int durat, int distance)
+		{
+			this.duration = durat;
+			this.date = day;
+			this.distance = distance;
+
+		}
+	}
+}
+
---
+++ Graficos/Graficos/Core/Diary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graficos
+{
+	public class Diary
+	{
+		List<BodyMeasures> measures;
+		List<Activity> exercise;
+
+		public Diary()
+		{
+			this.measures = new List<BodyMeasures>();
+			this.exercise = new List<Activity>();
+	
+		}
+
+		public List<Activity> GetActivities()
+		{
+			return this.exercise;
+		}
+		public List<BodyMeasures> GetMeasures()
+		{
+			return this.measures;
+		}
+		public void AddMeasure(BodyMeasures measure)
+		{
+			this.measures.Add(measure);
+			return;
+		}
+		public void AddExercise(Activity activity){
+			this.exercise.Add(activity);
+			return;
+		}
+		public Boolean DeleteExercise(Activity activity)
+		{
+			return this.exercise.Remove(activity);
+		}
+		public Boolean DeleteMeasure(BodyMeasures measure)
+		{
+			return this.measures.Remove(measure);
+		}
+
+		public override string ToString()
+		{
+			return string.Format("[Diary]"+ this.exercise.Count+", "+ this.measures.Count);
+		}
+	}
+}
